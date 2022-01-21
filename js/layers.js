2f52562fd9b21d8a9cbb9511093073d061e24440
theepichub <97768633+theepichub@users.noreply.github.com>
2022-01-21 21:32:26 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -168,7 +168,7 @@ addLayer("m", {
     baseResource: "superboops", // Name of resource prestige is based on
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.6, // Prestige currency exponent
+    exponent: 0.8, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
