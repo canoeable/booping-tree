f8a102c6fa31ed35906d0ee9da764d1141fbcaa2
theepichub <97768633+theepichub@users.noreply.github.com>
2022-01-21 20:10:14 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -246,3 +246,56 @@ addLayer("m", {
         },
     }
 })
+addLayer("b", {
+    name: "megaboosters", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "MB", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#047562",
+    requires: new Decimal(20), // Can be a function that takes requirement increases into account
+    resource: "megaboosters", // Name of prestige currency
+    baseResource: "megaboops", // Name of resource prestige is based on
+    baseAmount() {return player.m.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "b", description: "B: Reset for megaboosters", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){
+        if (player.m.points.gte(20)) {
+            return true
+        } else {
+            if (player[this.layer].total.gte(1)) {
+                return true
+            } else {
+                return false
+            }
+        } 
+    },
+    branches: 'm',
+    effect() {
+        if (Math.pow(2, player[this.layer].points).gte(1)) {
+            if (player[this.layer].points.times(3).gte(256)) {
+                return 256
+            } else {
+                return Math.pow(2, player[this.layer].points)
+            }
+        } else {
+            return 1
+        }
+    },
+    effectDescription() {
+        return "multiplying megaboop gain by " + format(tmp[this.layer].effect)
+    }
+})
